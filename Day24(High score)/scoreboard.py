da4d8f9e46790b7e2e2cefea65adcf89a2abd5a1
voidGlitch <official.shreshthav@gmail.com>
2021-08-04 21:02:55 +0530

    done some changes with the high score

--- Day24(High score)/scoreboard.py
+++ Day24(High score)/scoreboard.py
@@ -2,13 +2,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+with open("E:\\python on udemy\\Day24(High score)\\data.txt",mode="r") as file:
+    content = file.read()
 
+HIGH_SCORE = int(content)
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score=0
+        self.high_score= HIGH_SCORE
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("E:\\python on udemy\\Day24(High score)\\data.txt",mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
