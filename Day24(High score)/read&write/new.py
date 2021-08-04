e477572b4ade3969c1739edbc981536a80d8c42e
voidGlitch <official.shreshthav@gmail.com>
2021-08-04 20:43:29 +0530

    new branch is created

--- Day24(High score)/read&write/new.py
+++ Day24(High score)/read&write/new.py
@@ -8,6 +8,15 @@
 
 
 
-with open("E:\\python on udemy\\Day24(High score)\\read&write\\new_text.txt", mode="w") as File:
- '''by default the mode is readable only so we have to modify it and make it write'''
- File.write("some text.")  # delete all the prev content and overwrite in it
+# with open("E:\\python on udemy\\Day24(High score)\\read&write\\new_text.txt", mode="w") as File:
+#  '''by default the mode is readable only so we have to modify it and make it write'''
+#  File.write("some text.")  # delete all the prev content and overwrite in it
+
+# with open("E:\\python on udemy\\Day24(High score)\\read&write\\new_text.txt", mode="a") as File:
+#  '''by default the mode is readable only so we have to modify it and make it write or append(a)'''
+#  File.write("some text with some append mode.")  # append the text
+
+with open("E:\\python on udemy\\Day24(High score)\\read&write\\new_file.txt", mode="w") as File:
+ '''by default the mode is readable only so we have to modify it and make it write or append(a)'''
+ File.write("some text with some append mode.")  # append the text
+ # it will create new file if we don't have the existing file there automatically
